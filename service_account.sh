7b3934c02fa527cdd632a2b0f6acca045b64fd57
vyankatesh <v.boriwale@gmail.com>
2020-06-29 06:14:19 +0000

    service account scprit added

---
+++ service_account.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+PROJECT=$(gcloud config get-value project)
+PROJECT="cicdpipeline"
+SERVICE_ACCOUNT_NAME="gcp-terraform"
+
+#Create service account
+gcloud iam service-accounts create $SERVICE_ACCOUNT_NAME --display-name "gcp terraform service-accounts"
+
+#Check service account is create or not with given name
+gcloud iam service-accounts list --filter $SERVICE_ACCOUNT_NAME
+
+#Create key file, default extension is .json
+gcloud iam service-accounts keys create --iam-account $SERVICE_ACCOUNT_NAME@$PROJECT.iam.gserviceaccount.com $SERVICE_ACCOUNT_NAME".json"
+
+#assign role to newly created service account
+gcloud projects add-iam-policy-binding cicdpipeline --member "serviceAccount:$SERVICE_ACCOUNT_NAME@$PROJECT.iam.gserviceaccount.com" --role "roles/storage.admin"
+
+#Activate service account for use 
+gcloud auth activate-service-account --key-file=$SERVICE_ACCOUNT_NAME".json"
+
+#Set new serivice account as default to perform action
+gcloud config set account $SERVICE_ACCOUNT_NAME@$PROJECT.iam.gserviceaccount.com
+
+echo "################### New service account details as below ####################"
+echo "service account name : "$SERVICE_ACCOUNT_NAME
+echo "service account key name :"$SERVICE_ACCOUNT_NAME
+
