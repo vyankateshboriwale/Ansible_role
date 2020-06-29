5e7575552445e4479f4da806bf915862aafde1b0
vyankatesh <v.boriwale@gmail.com>
2020-06-29 07:24:44 +0000

    bucket creation code added

--- service_account.sh
+++ service_account.sh
@@ -2,7 +2,10 @@
 
 PROJECT=$(gcloud config get-value project)
 PROJECT="cicdpipeline"
-SERVICE_ACCOUNT_NAME="gcp-terraform"
+SERVICE_ACCOUNT_NAME="gcp-terraformtest"
+STORAGE="NEARLINE"
+LOCATION="us-central1-a"
+GCP_BUCKET="cards"
 
 #Create service account
 gcloud iam service-accounts create $SERVICE_ACCOUNT_NAME --display-name "gcp terraform service-accounts"
@@ -17,10 +20,15 @@ gcloud iam service-accounts keys create --iam-account $SERVICE_ACCOUNT_NAME@$PRO
 gcloud projects add-iam-policy-binding cicdpipeline --member "serviceAccount:$SERVICE_ACCOUNT_NAME@$PROJECT.iam.gserviceaccount.com" --role "roles/storage.admin"
 
 #Activate service account for use 
-gcloud auth activate-service-account --key-file=$SERVICE_ACCOUNT_NAME".json"
+#gcloud auth activate-service-account --key-file=$SERVICE_ACCOUNT_NAME".json"
 
 #Set new serivice account as default to perform action
-gcloud config set account $SERVICE_ACCOUNT_NAME@$PROJECT.iam.gserviceaccount.com
+#gcloud config set account $SERVICE_ACCOUNT_NAME@$PROJECT.iam.gserviceaccount.com
+
+#gsutil mb -p $PROJECT -c $storage_class -l $location -b on gs://$GCP_BUCKET/
+gsutil mb -p $PROJECT -l us-east1 gs://hsbccards/
+gsutil cp $SERVICE_ACCOUNT_NAME".json" gs://hsbccards
+gsutil ls gs://bucket
 
 echo "################### New service account details as below ####################"
 echo "service account name : "$SERVICE_ACCOUNT_NAME
